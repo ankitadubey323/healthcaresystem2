5f490b6c9bc9bb587fbf0b96fec853f8327eabf7
ankita <168554634+trustitgithub@users.noreply.github.com>
2026-04-18 18:01:25 +0530

    fix: remove nested folder

--- healthcare-main/back/routes/hospital.js
+++
@@ -1,9 +0,0 @@
-import express from 'express'
-import authMiddleware from '../middleware/auth.js'
-import { getNearbyHospitals } from '../controllers/hospitalController.js'
-
-const router = express.Router()
-
-router.get('/nearby', authMiddleware, getNearbyHospitals)
-
-export default router
